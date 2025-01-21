83977080edeb6c285f30147128dd94461b5d5dc5
aleixtm13 <aleix13t13m@gmail.com>
2025-01-21 14:46:04 +0100

    add: quiz2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,18 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = vec![];
+        for (s, c) in input {
+            match c {
+                Command::Uppercase => output.push(s.to_uppercase()),
+                Command::Trim => output.push(s.trim().to_string()),
+                Command::Append(n) => output.push(s + "bar".repeat(n).as_str()),
+            }
+        }
+        output
+
+    }
 }
 
 fn main() {
@@ -36,6 +48,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use crate::my_module::transformer;
+
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
